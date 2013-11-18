152e115643b70db0e1b24daf3c28456948055ef0
baldur <Baldur Gudbjornsson>
2013-11-17 22:54:37 -0500

    trying to leverage shadow stubs

--- src/test/java/com/mapzen/activity/BaseActivityTest.java
+++ src/test/java/com/mapzen/activity/BaseActivityTest.java
@@ -1,18 +1,23 @@
 package com.mapzen.activity;
 
 import android.app.Activity;
+import android.widget.SearchView;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.shadows.ShadowView;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
-public class MapActivityTest {
+public class BaseActivityTest {
     @Test
+    @Config(shadows = {SearchView.class, ShadowView.class})
     public void onCreate_initializesActivity() throws Exception {
         Activity activity = Robolectric.buildActivity(BaseActivity.class).create().get();
         assertTrue(activity != null);
     }
+
 }
