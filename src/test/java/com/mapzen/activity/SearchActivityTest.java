02e29b637722994ad29a857774accac2f568b33c
baldur <Baldur Gudbjornsson>
2013-11-20 11:11:55 -0500

    some testing adding

--- src/test/java/com/mapzen/activity/SearchActivityTest.java
+++ src/test/java/com/mapzen/activity/SearchActivityTest.java
@@ -1,22 +1,17 @@
 package com.mapzen.activity;
 
-import android.app.Activity;
-import android.widget.SearchView;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowView;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
-public class BaseActivityTest {
+public class SearchActivityTest {
     @Test
     public void onCreate_initializesActivity() throws Exception {
-        //BaseActivity activity = Robolectric.buildActivity(BaseActivity.class).create().get();
-        //1assertTrue(activity != null);
+        SearchActivity activity = Robolectric.buildActivity(SearchActivity.class).create().get();
+        assertTrue(activity != null);
     }
-
 }
