fd66d3ec6e00e88e36ecbd4ffb7af020095c481e
baldur <Baldur Gudbjornsson>
2013-12-02 10:21:17 -0500

    hookup sliding menu

--- app/src/main/java/com/mapzen/activity/VectorMapActivity.java
+++ app/src/main/java/com/mapzen/activity/VectorMapActivity.java
@@ -1,7 +1,11 @@
 package com.mapzen.activity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.mapzen.MapzenApplication;
 import com.mapzen.R;
 import org.oscim.android.MapActivity;
@@ -14,7 +18,7 @@ import org.oscim.tiling.source.TileSource;
 import org.oscim.tiling.source.oscimap4.OSciMap4TileSource;
 
 public class VectorMapActivity extends MapActivity {
-
+    private SlidingMenu slidingMenu;
     MapView mMapView;
     VectorTileLayer mBaseLayer;
 
@@ -22,7 +26,56 @@ public class VectorMapActivity extends MapActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vector);
+        setupActionbar();
+        setupSlidingMenu();
         setupMap();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if ( slidingMenu.isMenuShowing() ) {
+            slidingMenu.toggle();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ( keyCode == KeyEvent.KEYCODE_MENU ) {
+            this.slidingMenu.toggle();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.slidingMenu.toggle();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void setupSlidingMenu() {
+        slidingMenu = new SlidingMenu(this);
+        slidingMenu.setMode(SlidingMenu.LEFT);
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+        slidingMenu.setShadowWidthRes(R.dimen.slidingmenu_shadow_width);
+        slidingMenu.setShadowDrawable(R.drawable.slidingmenu_shadow);
+        slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        slidingMenu.setFadeDegree(0.35f);
+        slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_WINDOW);
+        slidingMenu.setMenu(R.layout.slidingmenu);
+    }
+
+    private void setupActionbar() {
+        ActionBar ab = getActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 
     private void setupMap() {
