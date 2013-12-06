fa9bd9cbd9af3391eb303cc1d86eac68632372bd
baldur <Baldur Gudbjornsson>
2013-12-06 12:59:40 -0500

    adds skeleton of an test for an activity

---
+++ app/src/test/java/com/mapzen/activity/BaseActivityTest.java
@@ -0,0 +1,28 @@
+package com.mapzen.activity;
+
+import com.mapzen.activity.BaseActivity;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.Robolectric;
+import android.app.Activity;
+
+import java.lang.Exception;
+
+@RunWith(RobolectricTestRunner.class)
+public class BaseActivityTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testOnCreate() throws Exception {
+        //Activity activity = Robolectric.buildActivity(BaseActivity.class).create().get();
+        // TODO sort out alien assets from VTM
+        assert(true);
+    }
+
+}
