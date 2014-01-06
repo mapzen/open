b995c4249faf6345a869b0857e6b2c7562ed277f
baldur <Baldur Gudbjornsson>
2014-01-06 13:47:26 -0500

    adding prioratized location

--- app/src/main/java/com/mapzen/MapzenApplication.java
+++ app/src/main/java/com/mapzen/MapzenApplication.java
@@ -18,16 +18,20 @@ import com.mapzen.util.Logger;
 import org.oscim.core.GeoPoint;
 import org.oscim.core.MapPosition;
 
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import static android.provider.BaseColumns._ID;
+import static com.mapzen.MapzenApplication.PriorityLocationListener.HIGH_PRIORITY;
+import static com.mapzen.MapzenApplication.PriorityLocationListener.LOW_PRIORITY;
+import static com.mapzen.MapzenApplication.PriorityLocationListener.MEDIUM_PRIORITY;
 
-public class MapzenApplication extends Application implements LocationListener {
+public class MapzenApplication extends Application {
     public static final int LOCATION_UPDATE_FREQUENCY = 1000;
     public static final float LOCATION_UPDATE_MIN_DISTANCE = 10.0f;
     public static final int HTTP_REQUEST_TIMEOUT_MS = 500;
     private static MapzenApplication app;
-    private Location location = null;
     public static final String PELIAS_TEXT = "text";
     public static final double DEFAULT_LATITUDE = 64.133333;
     public static final double DEFAULT_LONGITUDE = -21.933333;
@@ -39,6 +43,10 @@ public class MapzenApplication extends Application implements LocationListener {
     private RequestQueue queue;
 
     private LocationManager locationManager;
+    private HashMap<Integer, Location> location = new HashMap<Integer, Location>(3);
+    private LocationListener high_priority_listener = new PriorityLocationListener(HIGH_PRIORITY);
+    private LocationListener med_priority_listener = new PriorityLocationListener(MEDIUM_PRIORITY);
+    private LocationListener low_priority_listener = new PriorityLocationListener(LOW_PRIORITY);
 
     private final String[] columns = {
             _ID, PELIAS_TEXT
@@ -62,21 +70,26 @@ public class MapzenApplication extends Application implements LocationListener {
         app.queue = Volley.newRequestQueue(context);
         app.locationManager = (LocationManager)
                 context.getSystemService(Context.LOCATION_SERVICE);
-        app.setLocation(app.findBestLocation());
+        app.setLocation(PriorityLocationListener.LOW_PRIORITY, app.findBestLocation());
         return app;
     }
 
     public Location findBestLocation() {
         List<String> providers = locationManager.getProviders(true);
+        Logger.d("location: providers" + providers.toString());
         Location bestLocation = null;
         for (String provider : providers) {
+            Logger.d("location: provider: " + provider.toString());
             Location loc = locationManager.getLastKnownLocation(provider);
+            Logger.d("location: last known location: " + loc.toString());
             if (loc == null) {
+                Logger.d("location: is null");
                 continue;
             }
             if (bestLocation == null
                     || loc.getAccuracy() < bestLocation.getAccuracy()) {
                 bestLocation = loc;
+                Logger.d("location: bestLocation: " + bestLocation.toString());
             }
         }
         if (bestLocation == null) {
@@ -94,15 +107,24 @@ public class MapzenApplication extends Application implements LocationListener {
     }
 
     public Location getLocation() {
-        return location;
+        Location loc = location.get(0);
+        if (loc == null) {
+            loc = location.get(1);
+        }
+        if (loc == null) {
+            loc = location.get(2);
+        }
+        return loc;
     }
 
-    public void setLocation(Location location) {
-        Logger.d("Location: setting location");
+    public void setLocation(int weight, Location location) {
+        Logger.d("Location: setting location: weight: " +
+                String.valueOf(weight) + ", time: " + new Date().toString());
         if (location == null) {
             Logger.d("Location: location is null");
+        } else {
+            this.location.put(weight, location);
         }
-        this.location = location;
     }
 
     private String getBestProvider() {
@@ -115,31 +137,24 @@ public class MapzenApplication extends Application implements LocationListener {
     public void setupLocationUpdates() {
         Logger.d("Location: Requesting updates");
         locationManager.requestLocationUpdates(getBestProvider(),
-                LOCATION_UPDATE_FREQUENCY, LOCATION_UPDATE_MIN_DISTANCE, this);
+                LOCATION_UPDATE_FREQUENCY, LOCATION_UPDATE_MIN_DISTANCE,
+                high_priority_listener);
+        locationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER,
+                LOCATION_UPDATE_FREQUENCY, LOCATION_UPDATE_MIN_DISTANCE,
+                med_priority_listener);
+        locationManager.requestLocationUpdates(
+                LocationManager.PASSIVE_PROVIDER,
+                LOCATION_UPDATE_FREQUENCY, LOCATION_UPDATE_MIN_DISTANCE,
+                low_priority_listener);
     }
 
     public void stopLocationUpdates() {
         if (locationManager != null) {
-            locationManager.removeUpdates(this);
+            locationManager.removeUpdates(low_priority_listener);
+            locationManager.removeUpdates(med_priority_listener);
+            locationManager.removeUpdates(high_priority_listener);
         }
-    }
-
-    @Override
-    public void onLocationChanged(Location loc) {
-        Logger.d("Location: setting location: ");
-        setLocation(loc);
-    }
-
-    @Override
-    public void onStatusChanged(String s, int i, Bundle bundle) {
-    }
-
-    @Override
-    public void onProviderEnabled(String s) {
-    }
-
-    @Override
-    public void onProviderDisabled(String s) {
     }
 
     public GeoPoint getLocationPoint() {
@@ -181,5 +196,36 @@ public class MapzenApplication extends Application implements LocationListener {
         request.setRetryPolicy(new DefaultRetryPolicy(HTTP_REQUEST_TIMEOUT_MS,
                 DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         queue.add(request);
+    }
+
+    public static class PriorityLocationListener implements LocationListener {
+        public static final int HIGH_PRIORITY = 0;
+        public static final int MEDIUM_PRIORITY = 1;
+        public static final int LOW_PRIORITY = 2;
+        private int weight = 2;
+
+        public PriorityLocationListener(int weight) {
+            this.weight = weight;
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            app.setLocation(weight, location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
     }
 }
