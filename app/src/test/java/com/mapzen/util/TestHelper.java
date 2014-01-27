54e7363d7aa943d3c093bf0382eb08d19a76eefb
baldur <baldur@mapzen.com>
2014-01-27 14:09:08 -0500

    removes unused method

--- app/src/test/java/com/mapzen/util/TestHelper.java
+++ app/src/test/java/com/mapzen/util/TestHelper.java
@@ -18,21 +18,6 @@ public final class TestHelper {
     private TestHelper() {
     }
 
-    public static void simulateLocation(double lat, double lon) {
-        Location location = new Location(LocationManager.GPS_PROVIDER);
-        location.setLatitude(lat);
-        location.setLongitude(lon);
-        location.setProvider(LocationManager.GPS_PROVIDER);
-
-        LocationManager manager = (LocationManager)
-                Robolectric.application.getSystemService(Context.LOCATION_SERVICE);
-        ShadowLocationManager shadowManager = Robolectric.shadowOf(manager);
-        shadowManager.setProviderEnabled(LocationManager.GPS_PROVIDER, true);
-        shadowManager.setProviderEnabled(LocationManager.NETWORK_PROVIDER, true);
-        shadowManager.setProviderEnabled(LocationManager.PASSIVE_PROVIDER, true);
-        shadowManager.simulateLocation(location);
-    }
-
     public static BaseActivity initBaseActivity() {
         BaseActivity activity = Robolectric.buildActivity(BaseActivity.class).create().get();
         activity.onCreateOptionsMenu(new TestMenu());
