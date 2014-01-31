28ba46739680db0858e822c99d29864823b7f6e2
baldur <baldur@mapzen.com>
2014-01-31 09:49:06 -0500

    removing unused class

--- app/src/main/java/com/mapzen/fragment/RouteFragment.java
+++ app/src/main/java/com/mapzen/fragment/RouteFragment.java
@@ -10,7 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.android.volley.Response;
