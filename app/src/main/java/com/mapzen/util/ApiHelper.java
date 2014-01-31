7c3f80dabeea528f50e1cf1f97293aed8b16f242
baldur <baldur@mapzen.com>
2014-01-31 10:23:39 -0500

    changing urls

--- app/src/main/java/com/mapzen/util/ApiHelper.java
+++ app/src/main/java/com/mapzen/util/ApiHelper.java
@@ -10,14 +10,14 @@ public abstract class ApiHelper {
     public static final String JSON = "json";
     public static final String TRUE = "true";
 
-    public static final String ROUTE_URL = "api-osrm-test.mapzen.com";
+    public static final String ROUTE_URL = "osrm.test.mapzen.com";
     public static final String ROUTE_CAR_PATH = "car/viaroute";
     public static final String ROUTE_ZOOMLEVEL = "z";
     public static final String ROUTE_OUTPUT_KEY = "output";
     public static final String ROUTE_LOCATION_KEY = "loc";
     public static final String ROUTE_INSTRUCTIONS_KEY = "instructions";
 
-    public static final String PELIAS_URL = "api-pelias-test.mapzen.com";
+    public static final String PELIAS_URL = "pelias.test.mapzen.com";
     public static final String PELIAS_SUGGEST_PATH = "suggest";
     public static final String PELIAS_SEARCH_PATH = "search";
     public static final String PELIAS_QUERY_KEY = "query";
