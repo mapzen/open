867001967ad177b3dd3c08df25d8f0843dde67e6
baldur <baldur@mapzen.com>
2014-02-27 17:09:05 -0500

    adds TestLocation object which can generate location in specified distance

--- src/test/java/com/mapzen/support/TestHelper.java
+++ src/test/java/com/mapzen/support/TestHelper.java
@@ -1,18 +1,20 @@
 package com.mapzen.support;
 
+import com.mapzen.R;
+import com.mapzen.activity.BaseActivity;
+import com.mapzen.entity.Feature;
+import com.mapzen.fragment.MapFragment;
+
+import org.apache.commons.io.FileUtils;
+import org.oscim.android.MapView;
+import org.oscim.map.TestMap;
+import org.robolectric.tester.android.view.TestMenu;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentManager;
-import com.mapzen.R;
-import com.mapzen.activity.BaseActivity;
-import com.mapzen.entity.Feature;
-import com.mapzen.fragment.MapFragment;
-import org.apache.commons.io.FileUtils;
-import org.oscim.android.MapView;
-import org.oscim.map.TestMap;
-import org.robolectric.tester.android.view.TestMenu;
 
 import java.io.File;
 
@@ -87,5 +89,74 @@ public final class TestHelper {
         SharedPreferences.Editor prefEditor = prefs.edit();
         prefEditor.putBoolean(context.getString(R.string.settings_key_debug), true);
         prefEditor.commit();
+    }
+
+    public static class TestLocation extends Location {
+        private double distance;
+        private double bearing;
+        private double rLat, rLng;
+        private Location originalLocation;
+        private int R = 6371;
+
+        public TestLocation(String provider) {
+            super(provider);
+        }
+
+        private TestLocation(Builder builder) {
+            super("fake tester");
+            this.distance = builder.distance / R;
+            this.bearing = Math.toRadians(builder.bearing);
+            this.originalLocation = builder.originalLocation;
+            this.rLat = Math.toRadians(originalLocation.getLatitude());
+            this.rLng = Math.toRadians(originalLocation.getLongitude());
+            this.setLatitude(Math.toDegrees(getFarAwayLat()));
+            this.setLongitude(Math.toDegrees(getFarAwayLng()));
+        }
+
+        private double getFarAwayLat() {
+            double lat = Math.asin(Math.sin(rLat)
+                    * Math.cos(distance / R)
+                    + Math.cos(rLat * Math.sin(distance / R))
+                    * Math.cos(bearing));
+            return lat;
+        }
+
+        private double getFarAwayLng() {
+            double lng = rLng + Math.atan2(
+                    Math.sin(bearing)
+                            * Math.sin(distance)
+                            * Math.cos(rLat)
+                    ,
+                    Math.cos(distance)
+                            - Math.sin(rLat)
+                            * Math.sin(getFarAwayLat())
+            );
+
+            return lng;
+        }
+
+        public static class Builder {
+            private double distance;
+            private double bearing;
+            private Location originalLocation;
+
+            public Builder(Location location) {
+                this.originalLocation = location;
+            }
+
+            public Builder setDistance(double distance) {
+                this.distance = distance;
+                return this;
+            }
+
+            public Builder setBearing(double bearing) {
+                this.bearing = bearing;
+                return this;
+            }
+
+            public TestLocation build() {
+                return new TestLocation(this);
+            }
+        }
     }
 }
