1b705e291fbf4baaaf6570c289e7f751f3fe96e3
Chuck Greb <charles.greb@gmail.com>
2014-05-20 14:23:16 -0400

    Script install-vtm-native-libs.sh should clean up after itself.

--- scripts/install-vtm-native-libs.sh
+++ scripts/install-vtm-native-libs.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #
-# This script installs the VTM project and native dependencies.
+# This script installs native dependencies for VTM to the local Maven repo.
 #
 # Usage:
 #   install-vtm-native-libs.sh
 
 git clone https://github.com/mapzen/vtm-native-libs.git
 cd vtm-native-libs && ./install-dependencies.sh
+cd .. && rm -rf vtm-native-libs
