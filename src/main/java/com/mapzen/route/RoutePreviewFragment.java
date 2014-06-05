efe68659bf0175f6fd9cb7effe555ceef6ea6090
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2014-06-05 09:27:11 -0400

    removes unused import

--- src/main/java/com/mapzen/route/RoutePreviewFragment.java
+++ src/main/java/com/mapzen/route/RoutePreviewFragment.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
