0a559c91855065746c9de5c55645395824703fed
kingofirony <kingofirony@gmail.com>
2014-06-11 15:35:13 -0400

    adds tests for generating descriptions

--- src/main/java/com/mapzen/route/RouteFragment.java
+++ src/main/java/com/mapzen/route/RouteFragment.java
@@ -669,7 +669,7 @@ public class RouteFragment extends BaseFragment implements DirectionListFragment
     public String getGPXDescription() {
         if (instructions.size() >= 1) {
             Instruction firstInstruction = instructions.get(0);
-            String destination = simpleFeature.toString();
+            String destination = simpleFeature.getFullLocationString();
             return new StringBuilder().append("Route between: ")
                     .append(formatInstructionForDescription(firstInstruction))
                     .append(" -> ")
@@ -679,15 +679,28 @@ public class RouteFragment extends BaseFragment implements DirectionListFragment
         }
     }
 
-    private String formatInstructionForDescription(Instruction instruction) {
-        String locationString = instruction.toString().split("Instruction: ")[1];
-        String[] locationStringSplit = locationString.split("\\(.*,\\s.*\\)");
-        String finalString = locationStringSplit[1].replace(instruction.getHumanTurnInstruction(), "");
-        String latLong = " [" + instruction.getLocation().getLatitude() + ", " + instruction.getLocation().getLongitude() + ']';
-        Logger.d("final is " + latLong);
-        return finalString + latLong;
+    @Override
+    public String toString() {
+        if (instructions.size() >= 1) {
+            Instruction firstInstruction = instructions.get(0);
+            String destination = simpleFeature.toString();
+            return new StringBuilder().append("Route between: ")
+                    .append(firstInstruction.toString())
+                    .append(" -> ")
+                    .append(destination).toString();
+        } else {
+            return "Route without instructions";
+        }
     }
 
+    private String formatInstructionForDescription(Instruction instruction) {
+        Location loc = instruction.getLocation();
+        String locationName = instruction.getSimpleInstruction()
+                .replace(instruction.getHumanTurnInstruction(), "");
+        String latLong = " [" + loc.getLatitude() + ", " + loc.getLongitude() + ']';
+        String startLocationString = locationName + latLong;
+        return startLocationString;
+    }
 
     private void initDebugView(View view) {
         debugView = (DebugView) view.findViewById(R.id.debugging);
