50d4e20f6a500e7431fd1dae7b38d990307d884b
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2014-08-14 11:59:36 -0400

    adds mixpanel helper and quick intitial implemention in Initial activity

--- src/test/java/com/mapzen/activity/InitialActivityTest.java
+++ src/test/java/com/mapzen/activity/InitialActivityTest.java
@@ -1,26 +1,60 @@
 package com.mapzen.activity;
 
+import com.mapzen.MapzenApplication;
+import com.mapzen.TestMapzenApplication;
 import com.mapzen.support.MapzenTestRunner;
+
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
 import org.fest.assertions.api.Assertions;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 import org.scribe.model.Token;
 
+import javax.inject.Inject;
+
 import static com.mapzen.support.TestHelper.initBaseActivity;
 import static com.mapzen.support.TestHelper.initInitialActivity;
+import static com.mapzen.util.MixpanelHelper.Event.INITIAL_LAUNCH;
+import static com.mapzen.util.MixpanelHelper.Payload.LOGGED_IN_KEY;
 import static org.fest.assertions.api.ANDROID.assertThat;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.refEq;
+import static org.robolectric.Robolectric.application;
 import static org.robolectric.Robolectric.shadowOf;
 
 @Config(emulateSdk = 18)
 @RunWith(MapzenTestRunner.class)
 public class InitialActivityTest {
     private BaseActivity baseActivity;
+    @Inject MixpanelAPI mixpanelAPI;
 
     @Before
     public void setUp() throws Exception {
+        ((TestMapzenApplication) Robolectric.application).inject(this);
         baseActivity = initBaseActivity();
+    }
+
+    @Test
+    public void onCreate_shouldTrackAsLoggedOut() throws Exception {
+        initInitialActivity();
+        JSONObject expectedPayload = new JSONObject();
+        expectedPayload.put(LOGGED_IN_KEY, String.valueOf(false));
+        Mockito.verify(mixpanelAPI).track(eq(INITIAL_LAUNCH), refEq(expectedPayload));
+    }
+
+    @Test
+    public void onCreate_shouldTrackAsLoggedIn() throws Exception {
+        ((MapzenApplication) application).setAccessToken(new Token("hokus", "bogus"));
+        initInitialActivity();
+        JSONObject expectedPayload = new JSONObject();
+        expectedPayload.put(LOGGED_IN_KEY, String.valueOf(true));
+        Mockito.verify(mixpanelAPI).track(eq(INITIAL_LAUNCH), refEq(expectedPayload));
     }
 
     @Test
