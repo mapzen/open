aa644166c77efc9c63717193cfd800ca2d378df4
Chuck Greb <charles.greb@gmail.com>
2014-10-02 15:43:02 -0400

    Remove unused import.

--- src/test/java/com/mapzen/route/RouteAdapterTest.java
+++ src/test/java/com/mapzen/route/RouteAdapterTest.java
@@ -15,7 +15,6 @@ import android.text.SpannedString;
 import android.text.style.StyleSpan;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
