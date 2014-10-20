26db2750685f1f6701076bbad4244ebf997f39e7
Chuck Greb <charles.greb@gmail.com>
2014-10-20 19:28:24 -0400

    Fix path to keystore

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,7 +8,7 @@ if [ -z ${PERFORM_RELEASE} ]
     git config --global user.name "Circle CI"
     git config --global user.email "android-support@gmail.com"
     mvn clean release:clean release:prepare --batch-mode -DignoreSnapshots=true
-    git clone $SIGN_REPO ../config
+    git clone $SIGN_REPO ~/android-config
     scripts/production-values.sh
-    mvn release:perform --batch-mode -DignoreSnapshots=true -Darguments="-Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dsign.keystore=../config/$SIGN_KEYSTORE -Dsign.alias=$SIGN_ALIAS -Dsign.storepass=$SIGN_STOREPASS -Dsign.keypass=$SIGN_KEYPASS"
+    mvn release:perform --batch-mode -DignoreSnapshots=true -Darguments="-Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dsign.keystore=~/android-config/$SIGN_KEYSTORE -Dsign.alias=$SIGN_ALIAS -Dsign.storepass=$SIGN_STOREPASS -Dsign.keypass=$SIGN_KEYPASS"
 fi
