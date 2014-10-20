4dfd89e8072bfcb32e371b8f5da912884a1b1178
Chuck Greb <charles.greb@gmail.com>
2014-10-20 15:53:47 -0400

    Removes dry run

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,6 @@ if [ -z ${PERFORM_RELEASE} ]
     git reset --hard
     git config --global user.name "Circle CI"
     git config --global user.email "android-support@gmail.com"
-    mvn clean release:clean release:prepare --batch-mode -DignoreSnapshots=true -DdryRun=true
     mvn clean release:clean release:prepare --batch-mode -DignoreSnapshots=true
     git clone $SIGN_REPO ../config
     mvn release:perform --batch-mode -DignoreSnapshots=true -Darguments="-Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dsign.keystore=../config/$SIGN_KEYSTORE -Dsign.alias=$SIGN_ALIAS -Dsign.storepass=$SIGN_STOREPASS -Dsign.keypass=$SIGN_KEYPASS"
