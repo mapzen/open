c801a8eba857af52eaf587b038b2a5940e4da0de
Chuck Greb <charles.greb@gmail.com>
2014-10-20 18:37:12 -0400

    Moves production values script execution from pom.xml to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,5 +9,6 @@ if [ -z ${PERFORM_RELEASE} ]
     git config --global user.email "android-support@gmail.com"
     mvn clean release:clean release:prepare --batch-mode -DignoreSnapshots=true
     git clone $SIGN_REPO ../config
+    scripts/production-values.sh
     mvn release:perform --batch-mode -DignoreSnapshots=true -Darguments="-Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dsign.keystore=../config/$SIGN_KEYSTORE -Dsign.alias=$SIGN_ALIAS -Dsign.storepass=$SIGN_STOREPASS -Dsign.keypass=$SIGN_KEYPASS"
 fi
