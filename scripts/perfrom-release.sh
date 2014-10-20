ebac4c5ae7dbe54bee3201a872443af2ebab05de
Chuck Greb <charles.greb@gmail.com>
2014-10-20 12:07:36 -0400

    Adds script to perform release

---
+++ scripts/perfrom-release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ -z ${CIRCLE_TOKEN} ]
+  then
+    echo "[ERROR] Please set CIRCLE_TOKEN environment variable and rerun."
+    exit 1
+fi
+
+trigger_build_url=https://circleci.com/api/v1/project/mapzen/mapzen-android-demo/tree/master?circle-token=${CIRCLE_TOKEN}
+echo Triggering release build via $trigger_build_url
+
+post_data=$(cat <<EOF
+{
+  "build_parameters": {
+    "PERFORM_RELEASE": "true"
+  }
+}
+EOF)
+
+curl \
+--header "Accept: application/json" \
+--header "Content-Type: application/json" \
+--data "${post_data}" \
+--request POST ${trigger_build_url}
+
+echo
