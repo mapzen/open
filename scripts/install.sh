c758521954d651d084284a833f69d670eab181cf
Chuck Greb <charles.greb@gmail.com>
2014-10-20 21:10:07 -0400

    Hardcode keystore and alias since it gets echoed to the output anyway

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,5 +10,5 @@ if [ -z ${PERFORM_RELEASE} ]
     mvn clean release:clean release:prepare --batch-mode -DignoreSnapshots=true -Darguments="-Dmaven.test.skip=true"
     git clone $SIGN_REPO ~/android-config
     scripts/production-values.sh
-    mvn release:perform --batch-mode -DignoreSnapshots=true -Darguments="-Dmaven.test.skip=true -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dsign.keystore=~/android-config/$SIGN_KEYSTORE -Dsign.alias=$SIGN_ALIAS -Dsign.storepass=$SIGN_STOREPASS -Dsign.keypass=$SIGN_KEYPASS"
+    mvn release:perform --batch-mode -DignoreSnapshots=true -Darguments="-Dmaven.test.skip=true -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dsign.keystore=~/android-config/mapzen.keystore -Dsign.alias=mapzen_prod -Dsign.storepass=$SIGN_STOREPASS -Dsign.keypass=$SIGN_KEYPASS"
 fi
