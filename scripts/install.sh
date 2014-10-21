8292797cd995c2f78d8155b42fe6190d39372ec6
Chuck Greb <charles.greb@gmail.com>
2014-10-21 11:05:44 -0400

    Moves jarsigner from pom.xml to install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -10,5 +10,6 @@ if [ -z ${PERFORM_RELEASE} ]
     mvn clean release:clean release:prepare --batch-mode -DignoreSnapshots=true -Darguments="-Dmaven.test.skip=true"
     git clone $SIGN_REPO ~/android-config
     scripts/production-values.sh
-    mvn release:perform --batch-mode -DignoreSnapshots=true -Darguments="-Dmaven.test.skip=true -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dsign.keystore=~/android-config/mapzen.keystore -Dsign.alias=mapzen_prod -Dsign.storepass=$SIGN_STOREPASS -Dsign.keypass=$SIGN_KEYPASS"
+    mvn release:perform --batch-mode -DignoreSnapshots=true -Darguments="-Dmaven.test.skip=true -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET"
+    /usr/lib/jvm/jdk1.7.0/bin/jarsigner -verbose -keystore ~/android-config/mapzen.keystore -storepass "$SIGN_STOREPASS" -keypass "$SIGN_KEYPASS" target/*.apk mapzen_prod
 fi
