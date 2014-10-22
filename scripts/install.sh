4a31e626623fc7719e751195fb20a94d9c5df890
Chuck Greb <charles.greb@gmail.com>
2014-10-21 21:58:25 -0400

    Simplify release profile and enable automatic version code from version name.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ if [ -z ${PERFORM_RELEASE} ]
     mvn install -P jenkins -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dmaven.test.skip=true
   else
     ./scripts/production-values.sh
-    mvn install -P jenkins -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dmaven.test.skip=true
+    mvn install -P release -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dmaven.test.skip=true
     git clone $SIGN_REPO ~/android-config
     /usr/lib/jvm/jdk1.7.0/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/android-config/mapzen.keystore -storepass "$SIGN_STOREPASS" -keypass "$SIGN_KEYPASS" target/*.ap_ mapzen_prod
     mvn exec:exec -P zipalign
