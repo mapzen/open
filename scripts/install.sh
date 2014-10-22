37f23c58103b2dc52574fa5fd4fc4c37cdfe88b2
Chuck Greb <charles.greb@gmail.com>
2014-10-22 00:21:50 -0400

    Fix path when removing signature. Cleanup unsigned apk.

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,8 @@ if [ -z ${PERFORM_RELEASE} ]
     ./scripts/production-values.sh
     mvn install -P release -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dmaven.test.skip=true
     git clone $SIGN_REPO ~/android-config
-    zip -d *.apk "META-INF/*"
+    zip -d target/*.apk "META-INF/*"
     /usr/lib/jvm/jdk1.7.0/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/android-config/mapzen.keystore -storepass "$SIGN_STOREPASS" -keypass "$SIGN_KEYPASS" target/*.apk mapzen_prod
     mvn exec:exec -P zipalign
+    rm target/*-unsigned.apk
 fi
