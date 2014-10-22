ce89d5f0d3a3aae2664d8e0875be0703c33f40ce
Chuck Greb <charles.greb@gmail.com>
2014-10-22 01:01:47 -0400

    Re-enable tests on Circle CI

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,10 +2,10 @@
 
 if [ -z ${PERFORM_RELEASE} ]
   then
-    mvn install -P jenkins -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dmaven.test.skip=true
+    mvn install -P jenkins -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET
   else
     ./scripts/production-values.sh
-    mvn install -P release -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET -Dmaven.test.skip=true
+    mvn install -P release -Dmixpanel.token=$MIXPANEL_TOKEN -Dosm_oauth.key=$OSM_AUTH_KEY -Dosm_oauth.secret=$OSM_AUTH_SECRET
     git clone $SIGN_REPO ~/android-config
     zip -d target/*.apk "META-INF/*"
     /usr/lib/jvm/jdk1.7.0/bin/jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/android-config/mapzen.keystore -storepass "$SIGN_STOREPASS" -keypass "$SIGN_KEYPASS" target/*.apk mapzen_prod
