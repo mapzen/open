645aec0c4805053fbc1abd3e7e1bcff23f3029b8
Chuck Greb <charles.greb@gmail.com>
2015-02-06 17:18:10 -0500

    Install script cleanup

---
+++ scripts/dependency/install-support-jar_app.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# This script installs the Android Support v4 jar in your local Maven repository.
+#
+# Usage:
+#   install-support-jar_app.sh
+
+version="21.0.3"
+artifact="support_r$version"
+file="$artifact.zip"
+
+echo "Downloading $file"
+wget "https://dl-ssl.google.com/android/repository/$file"
+unzip "$file"
+
+echo "Installing $artifact"
+mvn -q install:install-file -DgroupId=com.android.support -DartifactId=support-v4 \
+  -Dversion="$version" -Dpackaging=jar -Dfile=support/v4/android-support-v4.jar
+
+echo "Deleting $file"
+rm "$file"
+
+echo "Deleting support/"
+rm -rf support
+
+echo "Done!"
