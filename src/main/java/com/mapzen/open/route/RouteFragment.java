a6fdbee7bd868073b1826ccfc90db15650be6042
Chuck Greb <charles.greb@gmail.com>
2015-04-02 00:17:49 -0400

    Checkstyle

--- src/main/java/com/mapzen/open/route/RouteFragment.java
+++ src/main/java/com/mapzen/open/route/RouteFragment.java
@@ -186,9 +186,11 @@ public class RouteFragment extends BaseFragment implements DirectionListFragment
         if (LocationServices.FusedLocationApi != null) {
             if (prefs.getBoolean(getString(R.string.settings_mock_gpx_key), false)) {
                 final String key = getString(R.string.settings_mock_gpx_filename_key);
-                final String defaultFile = getString(R.string.settings_mock_gpx_filename_default_value);
+                final String defaultFile =
+                        getString(R.string.settings_mock_gpx_filename_default_value);
                 final String filename = prefs.getString(key, defaultFile);
-                final File file = new File(Environment.getExternalStorageDirectory(), filename);
+                final File file = new File(Environment.getExternalStorageDirectory(),
+                        filename);
                 LocationServices.FusedLocationApi.setMockMode(true);
                 LocationServices.FusedLocationApi.setMockTrace(file);
             } else {
